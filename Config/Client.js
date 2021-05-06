da7693ddcddfb00729e53e11929e3fc2e7c68e2e
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-05-06 03:01:53 +0300

    Update Client.js

--- Config/Client.js
+++ Config/Client.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MAIN_URL: "http://localhost:8080/",
+  MAIN_URL: "http://localhost:4200/",
   ACCOUNT_VERFICATION_ROUTE: "/account-verification",
   PASSWORD_RESET_ROUTE: "/password-reset",
   JWT_QUERY_STRING: "?token=",
